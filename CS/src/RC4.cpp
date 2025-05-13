1ab9014676665b09724f8171577e3affde85c598
omar-alshamasneh <omaralshamasneh@gmail.com>
2025-05-13 22:00:36 +0300

    blablablablobleble

--- CS/src/RC4.cpp
+++ CS/src/RC4.cpp
@@ -1,52 +1,58 @@
 #include "RC4.h"
-#include <algorithm>
 #include <iostream>
 #include <iomanip>
 
-RC4::RC4(const std::string& key) {
-    keylen = static_cast<int>(key.length());
-    KSA(key);
+RC4::RC4(const std::string& key) : key(key), S(256), i(0), j(0) {
+    KSA();
 }
 
-void RC4::KSA(const std::string& key) {
-    S.resize(256);
-    for (int i = 0; i < 256; ++i) {
+void RC4::KSA() {
+    for (int i = 0; i < 256; ++i)
         S[i] = i;
-    }
 
     int j = 0;
     for (int i = 0; i < 256; ++i) {
-        j = (j + S[i] + static_cast<unsigned char>(key[i % keylen])) % 256;
+        j = (j + S[i] + static_cast<unsigned char>(key[i % key.length()])) % 256;
         std::swap(S[i], S[j]);
     }
+
+    i = j = 0;
 }
 
-std::vector<unsigned char> RC4::PRGA(const std::string& data) {
-    int i = 0, j = 0;
-    std::vector<unsigned char> result;
-    std::vector<int> S_local = S; // Copy to keep original
+unsigned char RC4::PRGA() {
+    i = (i + 1) % 256;
+    j = (j + S[i]) % 256;
+    std::swap(S[i], S[j]);
+    return S[(S[i] + S[j]) % 256];
+}
 
-    for (size_t k = 0; k < data.size(); ++k) {
-        i = (i + 1) % 256;
-        j = (j + S_local[i]) % 256;
-        std::swap(S_local[i], S_local[j]);
-        int rnd = S_local[(S_local[i] + S_local[j]) % 256];
-        result.push_back(data[k] ^ rnd);
-
-        std::cout << "K matrix after processing byte " << k + 1 << ": ";
-        for (int m = 0; m < 16; ++m)
-            std::cout << std::setw(3) << S_local[m] << " ";
-        std::cout << "...\n";
-    }
-
-    return result;
+void RC4::reset() {
+    S.resize(256);
+    i = j = 0;
+    KSA();
 }
 
 std::string RC4::encrypt(const std::string& plaintext) {
-    std::vector<unsigned char> cipher = PRGA(plaintext);
-    return std::string(cipher.begin(), cipher.end());
+    reset();
+    std::string ciphertext;
+
+    for (unsigned char c : plaintext) {
+        unsigned char k = PRGA();
+        ciphertext += c ^ k;
+        printState();
+    }
+
+    return ciphertext;
 }
 
 std::string RC4::decrypt(const std::string& ciphertext) {
-    return encrypt(ciphertext); // RC4 is symmetric
+    return encrypt(ciphertext); // same function for decryption
+}
+
+void RC4::printState() {
+    std::cout << "Key matrix (K): ";
+    for (int idx = 0; idx < 16; ++idx)
+        std::cout << std::setw(2) << std::setfill('0') << std::hex
+        << std::uppercase << static_cast<int>(S[idx]) << " ";
+    std::cout << std::dec << "\n";
 }
--- CS/src/RC4.h
+++ CS/src/RC4.h
@@ -1,4 +1,3 @@
-#pragma once
 #ifndef RC4_H
 #define RC4_H
 
@@ -7,15 +6,20 @@
 
 class RC4 {
 private:
-    std::vector<int> S;
-    int keylen;
-    void KSA(const std::string& key);
-    std::vector<unsigned char> PRGA(const std::string& data);
+    std::vector<unsigned char> S;
+    std::string key;
+
+    void KSA();
+    unsigned char PRGA();
+    void reset();
+
+    int i, j;
 
 public:
     RC4(const std::string& key);
     std::string encrypt(const std::string& plaintext);
     std::string decrypt(const std::string& ciphertext);
+    void printState(); // Print first 16 bytes of S
 };
 
 #endif
